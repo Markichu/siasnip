7a5c079c052b7172999fc213fe17538b079ff201
Markichu <31204091+Markichu@users.noreply.github.com>
2020-03-08 17:22:31 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # siasnip
 A snipping tool that uploads directly to Sia Skynet.
+### Dependecies
+- pyperclip
+- configparser
+- siaskynet
+- playsound
+- PyQt5
+- Pillow
+- desktopmagic
+
+install dependencies using:
+`pip install [dependency name]`
+
+### Usage
+Running siasnip.py will start the snipping tool, dragging a rectangle will then capture that part of the screen and upload it to Sia Skynet.
+
+If desired, siasnip.py can be renamed to siasnip.pyw (to run without a terminal) and can be opened however the user desires (e.g Autohotkey/batch script).
+
+### Config
+Config information is saved in the cfg.ini, each affects the function of siasnip: 
+- uploadportaladdress: portal address that will be used to upload the screengrab to skynet
+- linkportaladdress: portal address that is appended to the start of the skylink when added to the clipboard
+- beepfilename: filename for the .mp3/.wav that is played when the file is uploaded
---
+++ siasnip.py
@@ -0,0 +1,85 @@
+import sys
+import pyperclip
+import configparser
+import ctypes
+from siaskynet import Skynet
+from playsound import playsound
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PIL import Image
+from desktopmagic.screengrab_win32 import saveRectToBmp
+
+config = configparser.ConfigParser()
+config.read('cfg.ini')
+
+uploadportal = config['config']['uploadportaladdress']
+linkportal = config['config']['linkportaladdress']
+beepname = config['config']['beepfilename']
+
+def upload(file):
+    # upload file to skynet
+    print('uploading to skynet')
+    opts = Skynet.default_upload_options
+    opts.portalUrl = uploadportal
+    skylink = Skynet.UploadFile(file)
+    # put link into clipboard
+    pyperclip.copy(linkportal + '/' + Skynet.strip_prefix(skylink))
+    print('upload finished')
+    # beep
+    playsound(beepname, False)
+
+class SnipWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(SnipWindow, self).__init__()
+
+        # setup QtWidget to cover whole desktop
+        screen_width = ctypes.windll.user32.GetSystemMetrics(78)
+        screen_height = ctypes.windll.user32.GetSystemMetrics(79)
+        self.setGeometry(0, 0, screen_width, screen_height)
+        self.setWindowTitle(' ')
+        self.setWindowOpacity(0.3)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CrossCursor))
+
+        # set class for start/end point
+        self.begin = QtCore.QPoint()
+        self.end = QtCore.QPoint()
+        
+        self.show()
+
+    def paintEvent(self, event):
+        rect = QtGui.QPainter(self)
+        rect.setPen(QtGui.QPen(QtGui.QColor(0, 0, 0, 128), 2))
+        rect.setBrush(QtGui.QColor(128, 128, 128, 128))
+        rect.drawRect(QtCore.QRect(self.begin, self.end))
+
+    def mousePressEvent(self, event):
+        self.begin = event.pos()
+        self.end = self.begin
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.end = event.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        self.close()
+
+        # find top right and bottom left
+        x1 = min(self.begin.x(), self.end.x())
+        y1 = min(self.begin.y(), self.end.y())
+        x2 = max(self.begin.x(), self.end.x())
+        y2 = max(self.begin.y(), self.end.y())
+
+        # save copy of screen and call upload
+        saveRectToBmp('capture.bmp', rect=(x1, y1, x2, y2))
+        Image.open('capture.bmp').save('capture.png')
+        upload('capture.png')
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+
+    snippingTool = SnipWindow()
+    snippingTool.show()
+    
+    app.aboutToQuit.connect(app.deleteLater)
+    sys.exit(app.exec_())
